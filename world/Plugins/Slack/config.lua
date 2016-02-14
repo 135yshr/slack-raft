98991748a003d0b10deebc8db417d46b083ba601
135yshr <isago@praisefirst.jp>
2016-02-14 20:29:26 +0900

    mod Ground pos

--- world/Plugins/Slack/config.lua
+++ world/Plugins/Slack/config.lua
@@ -2,8 +2,8 @@
 -- for the Docker plugin.
 
 -- X,Z positions to draw first container
-CONTAINER_START_X = -3
-CONTAINER_START_Z = 2
+CONTAINER_START_X = 0
+CONTAINER_START_Z = 0
 -- offset to draw next container
 CONTAINER_OFFSET_X = -6
 
@@ -13,10 +13,10 @@ CONTAINER_OFFSET_X = -6
 GROUND_LEVEL = 63
 
 -- defines minimum surface to place one container
-GROUND_MIN_X = CONTAINER_START_X - 2
-GROUND_MAX_X = CONTAINER_START_X + 5
-GROUND_MIN_Z = -4
-GROUND_MAX_Z = CONTAINER_START_Z + 6
+GROUND_MIN_X = CONTAINER_START_X 
+GROUND_MAX_X = CONTAINER_START_X + 100
+GROUND_MIN_Z = CONTAINER_START_Z
+GROUND_MAX_Z = CONTAINER_START_Z + 100
 
 -- block updates are queued, this defines the 
 -- maximum of block updates that can be handled
